4f5f5174dd8b994bdb1da6192ce4a9538ff65175
Hristo Boyadzhiev <hristo_boyadzhiev@abv.bg>
2024-10-15 11:54:19 +0300

    update

--- tip-calculator/src/components/Footer/Footer.tsx
+++ tip-calculator/src/components/Footer/Footer.tsx
@@ -15,7 +15,6 @@ export default function Footer() {
       }}
     >
       <Typography>
-        {/* Challenge by{" "} */}
         {translate("footer.challenge-by")}
         <Link
           href="https://www.frontendmentor.io/challenges/tip-calculator-app-ugJNGbJUX"
@@ -31,34 +30,3 @@ export default function Footer() {
     </Box>
   );
 }
-
-// import { useIsSmallScreen } from "@/hooks/useIsSmallScreen";
-// import { Box, Link, Typography } from "@mui/material";
-
-// export default function Footer() {
-//   const isSmallScreen = useIsSmallScreen();
-//   return (
-//     <Box
-//       sx={{
-//         display: "flex",
-//         flexDirection: isSmallScreen ? "column" : "row",
-//         gap: isSmallScreen ? 0 : "0.2em",
-//         alignItems: isSmallScreen ? "center" : "flex-start",
-//       }}
-//     >
-//       <Typography>
-//         Challenge by{" "}
-//         <Link
-//           href="https://www.frontendmentor.io/challenges/tip-calculator-app-ugJNGbJUX"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//           sx={{ textDecoration: "none" }}
-//         >
-//           Frontend Mentor
-//         </Link>
-//         .
-//       </Typography>
-//       <Typography> Coded by Hristo Boyadzhiev.</Typography>
-//     </Box>
-//   );
-// }
